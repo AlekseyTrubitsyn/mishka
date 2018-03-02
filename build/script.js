3c6c2b555aecd53bed4de1d778bcf0dbfa0e415e
Aleksey Trubitsyn <at.8441451@yandex.ru>
2018-03-02 12:48:44 +0300

    create popup, fix header styles

--- build/script.js
+++ build/script.js
@@ -2,9 +2,43 @@
   const toggleButton = document.getElementById('menu-toggle');
   const menu = document.getElementById('menu');
 
+  const orderButton = document.getElementById('order-button');
+  const popup = document.getElementById('popup');
+  const popupForm = document.getElementById('popup-form');
+  const popupOverlay = document.getElementById('popup-overlay');
+  const popupCloseButton = document.getElementById('popup-close');
+
   toggleButton.addEventListener('click', (event) => {
     event.preventDefault();
 
     menu.classList.toggle('header__menu--expanded');
-  })
+  });
+
+  if (!(orderButton && popup && popupForm && popupOverlay && popupCloseButton)) return;
+
+  orderButton.addEventListener('click', showPopup);
+  popupOverlay.addEventListener('click', closePopup);
+  popupCloseButton.addEventListener('click', closePopup);
+
+  popupForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    alert('Добавлено!');
+    closePopup();
+  });
+
+  function closePopup(event) {
+    if (event) event.preventDefault();
+
+    popup.classList.add('hide');
+    popupOverlay.classList.add('hide');
+  }
+
+  function showPopup(e) {
+    console.log('showPopup');
+    e.preventDefault();
+
+    popup.classList.remove('hide');
+    popupOverlay.classList.remove('hide');
+  }
 })();
